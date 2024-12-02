f164ef88b01c73d4f2cf5f4fc96f9df60e205086
Robin Caulier <robincaulier3@gmail.com>
2024-12-02 14:41:36 +0100

    js modal

--- js/main.js
+++ js/main.js
@@ -29,22 +29,65 @@ function findCountries() {
             // Dynamisch de country weergeven in cards
             let outputHTML = '';
             countries.forEach(country => {
+                // Controleer of het land currencies heeft
+                let currencyInfo = '';
+                if (country.currencies) {
+                    // Itereer door de currencies
+                    const currencyKeys = Object.keys(country.currencies);
+                    currencyInfo = currencyKeys.map(key => {
+                        const currency = country.currencies[key];
+                        return `${currency.name} (${currency.symbol || 'Geen symbool'})`;
+                    }).join(', ');
+                } else {
+                    currencyInfo = 'No currency info available.';
+                }
+                //const currencyInfo = country.currencies ? Object.keys(country.currencies).join(', ') : 'No currency available!'
+                const languages = country.languages ? Object.values(country.languages).join(', ') : 'No languages available!';
                 outputHTML += `
                     <div class="col">
                         <!--Card-->
                         <article class="card">
                             <!--De vlag-->
-                            <img src="${country.flags.svg}" class="card-img-top" alt="">
+                            <img src="${country.flags.svg}" class="card-img-top" alt="country_flag">
                             <div class="card-body">
                                 <h5 class="card-title">${country.name.common}</h5>
                                 <p class="card-text">Continent: ${country.continents} </p>
                                 <p class="card-text">Population: ${country.population.toLocaleString()} </p>
                                 <!-- Button trigger modal -->
-                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#${country.cca3}">
                                     Meer info
                                 </button>
                             </div>
                         </article>
+                    </div>
+                    
+                    <div class="modal fade" id="${country.cca3}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-xl">
+                            <div class="modal-content bg-secondary">
+                                <div class="modal-header">
+                                    <h1 class="modal-title fs-5" id="exampleModalLabel">${country.name.common}</h1>
+                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body d-flex">
+                                    <!--leaflet map-->
+                                    <div class="modal-kaart">
+                                        <!-- Hier komt de leaflet map -->
+                                    </div>
+                                    <!--land info-->
+                                    <div class="modal-info">
+                                        <p>Capitol: ${country.capital}</p>
+                                        <p>Language: ${languages}</p>
+                                        <p>currency: ${currencyInfo}</p>
+                                        <p>Population: ${country.population.toLocaleString()}</p>
+                                        <img class="img-modal bg-secondary" src="${country.flags.svg}" alt="country_flag">
+                                    </div>
+                    
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 `;
             });
